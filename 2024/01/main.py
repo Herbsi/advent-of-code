037eeec5fe52735f09811c5a4e61a73ba917d29b
Herbsi <git@hoehenberger.me>
2024-12-01 18:17:37 +0100

    2024/01 – Part 2

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+from collections import Counter
+
 import numpy as np
+import operator as op
 import re
 
 
@@ -9,6 +12,8 @@ def parse_line(line):
         [int(group) for group in re.match(r"(\d+)\s+(\d+)", line).group(1, 2)]
     )
 
+
+## Part 1 ######################################################################
 
 with open("input.txt", "r") as f:
     print(
@@ -20,3 +25,12 @@ with open("input.txt", "r") as f:
             )
         )
     )
+
+
+## Part 2 ######################################################################
+
+with open("input.txt", "r") as f:
+    [left, right] = np.transpose(np.array([parse_line(line) for line in f]))
+    keys = set(left)
+    counter = Counter(right)
+    print(np.sum(np.fromiter((k * counter[k] for k in keys), dtype=np.int32)))
