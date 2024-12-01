88609e87feb01e3dcf5d801715f4b02ca05c0d59
Herbsi <git@hoehenberger.me>
2024-12-01 18:22:19 +0100

    2024/01 – Cleanup

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -7,30 +7,25 @@ import operator as op
 import re
 
 
-def parse_line(line):
-    return np.array(
-        [int(group) for group in re.match(r"(\d+)\s+(\d+)", line).group(1, 2)]
-    )
+def parse_input(filename):
+    with open(filename, "r") as f:
+        return np.array(
+            [
+                [int(group) for group in re.match(r"(\d+)\s+(\d+)", line).group(1, 2)]
+                for line in f
+            ]
+        )
 
+
+inp = parse_input("input.txt")
 
 ## Part 1 ######################################################################
 
-with open("input.txt", "r") as f:
-    print(
-        np.sum(
-            np.abs(
-                np.apply_along_axis(
-                    np.diff, 1, np.sort(np.array([parse_line(line) for line in f]), 0)
-                )
-            )
-        )
-    )
+print(np.sum(np.abs(np.apply_along_axis(np.diff, 1, np.sort(inp, 0)))))
 
 
 ## Part 2 ######################################################################
 
-with open("input.txt", "r") as f:
-    [left, right] = np.transpose(np.array([parse_line(line) for line in f]))
-    keys = set(left)
-    counter = Counter(right)
-    print(np.sum(np.fromiter((k * counter[k] for k in keys), dtype=np.int32)))
+keys = set(inp[:, 0])
+counter = Counter(inp[:, 1])
+print(np.sum(np.fromiter((k * counter[k] for k in keys), dtype=np.int32)))
