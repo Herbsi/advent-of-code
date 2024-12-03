5a8a6ec125a4cd10b81dae4dfacf8a9362311d33
Herbsi <git@hoehenberger.me>
2024-12-03 09:26:55 +0100

    2024/03 – Part 2

--- 2024/03/main.py
+++ 2024/03/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from collections import deque
+
 import numpy as np
 import re
 
@@ -11,3 +13,24 @@ with open("input.txt", "r") as f:
 ## Part 1 ######################################################################
 
 print(np.sum([int(a) * int(b) for (a, b) in mul_instr_re.findall(memory)]))
+
+
+## Part 2 ######################################################################
+
+instr_re = re.compile(r"(?:mul\(\d{1,3},\d{1,3}\))|(?:do\(\))|(?:don't\(\))")
+instr_stack = deque((match.group(0) for match in instr_re.finditer(memory)))
+
+active = True
+result = 0
+while instr_stack:
+    match instr_stack.popleft():
+        case "do()":
+            active = True
+        case "don't()":
+            active = False
+        case instr:
+            if active == True:
+                (a, b) = mul_instr_re.match(instr).groups((1, 2))
+                result += int(a) * int(b)
+
+print(result)
