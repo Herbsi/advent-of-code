702c942c458d165e160fe888106dfa924296726d
Herbsi <git@hoehenberger.me>
2024-12-07 16:54:05 +0100

    2024/07 – Part 2

--- 2024/07/main.py
+++ 2024/07/main.py
@@ -6,7 +6,7 @@ from collections import deque
 
 filename = "input.txt"
 
-operators = ["+", "*"]
+operators = ["+", "*", "||"]
 
 
 def parse_line(line):
@@ -36,10 +36,35 @@ def build_expression(values, operators):
 
 total_calibration_result = 0
 for test_value, values in equations:
-    for ops in itertools.product(operators, repeat=len(values) - 1):
-        value = eval(build_expression(values, ops))
+    for ops in itertools.product(operators[0:2], repeat=len(values) - 1):
+        value = eval("".join(build_expression(values, ops)))
         if test_value == value:
             total_calibration_result += test_value
             break
+
+print(total_calibration_result)
+
+## Part 2 ######################################################################
+
+total_calibration_result = 0
+for test_value, values in equations:
+    for ops in itertools.product(operators, repeat=len(values) - 1):
+        value = int(values[0])
+        current_value = None
+        for i in range(0, len(values) - 1):
+            op = ops[i]
+            current_value = values[i + 1]
+            match op:
+                case "+":
+                    value += int(current_value)
+                case "*":
+                    value *= int(current_value)
+                case "||":
+                    value = int("".join([str(value), current_value]))
+
+        if value == test_value:
+            total_calibration_result += test_value
+            break
+
 
 print(total_calibration_result)
