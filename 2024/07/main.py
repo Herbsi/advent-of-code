d51083d6e47a9132a87baafec3faad7da16f2911
Herbsi <git@hoehenberger.me>
2024-12-07 18:33:04 +0100

    2024/07 – Cleanup

--- 2024/07/main.py
+++ 2024/07/main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
+import functools
 import itertools
 import re
 from collections import deque
 
 filename = "input.txt"
-
-operators = ["+", "*", "||"]
 
 
 def parse_line(line):
@@ -14,57 +13,51 @@ def parse_line(line):
 
     test_value, values = equation_re.match(line).groups()
 
-    return (int(test_value), values.split(" ")[1:])
+    return (int(test_value), [int(value) for value in values.split(" ")[1:]])
 
 
 with open(filename, "r") as f:
     equations = [parse_line(line) for line in f.read().split("\n")[:-1]]
 
 
-def build_expression(values, operators):
-    values = deque(values)
-    operators = deque(operators)
-    expr = ["".join(["("] * (len(values) - 1)), values.popleft()]
-
-    while len(values) != 0:
-        expr.append(operators.popleft())
-        expr.append(values.popleft())
-        expr.append(")")
-
-    return "".join(expr)
+operators = ["+", "*", "||"]
 
 
-total_calibration_result = 0
-for test_value, values in equations:
-    for ops in itertools.product(operators[0:2], repeat=len(values) - 1):
-        value = eval("".join(build_expression(values, ops)))
-        if test_value == value:
-            total_calibration_result += test_value
-            break
+def process_equation(test_value, values, operators=["+", "*"]):
+    def folder(acc, cur):
+        match cur[1]:
+            case "+":
+                return acc + cur[0]
+            case "*":
+                return acc * cur[0]
+            case "||":
+                return int(f"{acc}{cur[0]}")
 
-print(total_calibration_result)
+    for ops in itertools.product(operators, repeat=len(values) - 1):
+        if functools.reduce(folder, zip(values[1:], ops), values[0]) == test_value:
+            return test_value
+    else:
+        return None
+
+
+print(
+    sum(
+        (
+            v
+            for (test_value, values) in equations
+            if (v := process_equation(test_value, values))
+        )
+    )
+)
 
 ## Part 2 ######################################################################
 
-total_calibration_result = 0
-for test_value, values in equations:
-    for ops in itertools.product(operators, repeat=len(values) - 1):
-        value = int(values[0])
-        current_value = None
-        for i in range(0, len(values) - 1):
-            op = ops[i]
-            current_value = values[i + 1]
-            match op:
-                case "+":
-                    value += int(current_value)
-                case "*":
-                    value *= int(current_value)
-                case "||":
-                    value = int("".join([str(value), current_value]))
-
-        if value == test_value:
-            total_calibration_result += test_value
-            break
-
-
-print(total_calibration_result)
+print(
+    sum(
+        (
+            v
+            for (test_value, values) in equations
+            if (v := process_equation(test_value, values, ["+", "*", "||"]))
+        )
+    )
+)
