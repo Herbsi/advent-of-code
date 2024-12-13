4e0eed79bdc82a883ed24b78fa8b7327dca05de6
Herbsi <git@hoehenberger.me>
2024-12-13 12:32:36 +0100

    2024/09 – Part 2

--- 2024/09/main.lisp
+++ 2024/09/main.lisp
@@ -17,7 +17,6 @@
   (iter:iter
     (with disk-map = (map 'vector #'digit-char-p disk-map))
     (with disk = (make-hash-table))
-    (with disk-space-map = (make-hash-table))
 
     (for file-id upfrom 0)
     (for file-blocks in-vector disk-map by 2)
@@ -34,9 +33,7 @@
 
     (when (> space-blocks 0)
       (setf (gethash space-position disk) disk-space)
-      (collect (list space-position disk-space) into disk-space-queue)
-      (setf (gethash space-blocks disk-space-map)
-            (append (gethash space-blocks disk-space-map) (list space-position))))
+      (collect (list space-position disk-space) into disk-space-queue))
 
     (finally
      (let* ((blocks (aref disk-map (1- (length disk-map))))
@@ -44,10 +41,7 @@
             (position (+ file-position space-blocks)))
        (setf (gethash position disk) file)
        (push (list position file) file-queue))
-     (return (list disk file-queue disk-space-queue disk-space-map)))))
-
-
-(parse-disk-map (uiop:read-file-line "test.txt"))
+     (return (list disk file-queue disk-space-queue)))))
 
 
 (defun move-block (disk file-queue disk-space-queue)
@@ -95,15 +89,61 @@
 (defun checksum (disk)
   (iter
     (for (position file) in-hashtable disk)
-    (sum (* (file-id file)
-            (file-blocks file)
-            (+ position (/ (1- (file-blocks file)) 2))))))
+    (when (file-p file)
+      (sum (* (file-id file)
+              (file-blocks file)
+              (+ position (/ (1- (file-blocks file)) 2)))))))
 
 
 (defun part-1 (filename)
-  (destructuring-bind (disk file-queue disk-space-queue disk-space-map)
-      (parse-disk-map (uiop:read-file-line filename))
-    (checksum (compact disk file-queue disk-space-queue))))
+  (checksum (apply #'compact (parse-disk-map (uiop:read-file-line filename)))))
 
 
 (part-1 "input.txt") ; => 6607511583593 (43 bits, #x6026E617B69)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun move-block-2 (disk file-queue disk-space-queue)
+  (trivia:match (car file-queue)
+    ((list file-position (file :id file-id :blocks file-blocks))
+     (alexandria:if-let ((idx
+                          (position-if (lambda (disk-space)
+                                         (and
+                                          (< (car disk-space) file-position)
+                                          (<= file-blocks (disk-space-blocks (cadr disk-space)))))
+                                       disk-space-queue)))
+       (trivia:match (nth idx disk-space-queue)
+         ((list space-position (disk-space :blocks space-blocks))
+          (remhash file-position disk)
+          (cond ((< file-blocks space-blocks)
+                 (let ((new-space-position (+ space-position file-blocks))
+                       (remaining-space (make-disk-space :blocks (- space-blocks file-blocks))))
+                   (setf (gethash space-position disk) (make-file :id file-id :blocks file-blocks))
+                   (setf (gethash new-space-position disk) remaining-space)
+                   (list disk
+                         (cdr file-queue)
+                         (append (subseq disk-space-queue 0 idx)
+                                 `(,(list new-space-position remaining-space))
+                                 (subseq disk-space-queue (1+ idx) nil)))))
+                ((= file-blocks space-blocks)
+                 (setf (gethash space-position disk) (make-file :id file-id :blocks file-blocks))
+                 (list disk
+                       (cdr file-queue)
+                       (append (subseq disk-space-queue 0 idx)
+                               (subseq disk-space-queue (1+ idx) nil)))))))
+       (list disk (cdr file-queue) disk-space-queue)))))
+
+
+(defun compact-2 (disk file-queue disk-space-map)
+  (labels ((rec (disk file-queue disk-space-map)
+             (if file-queue
+                 (apply #'rec (move-block-2 disk file-queue disk-space-map))
+                 disk)))
+    (rec disk file-queue disk-space-map)))
+
+
+(defun part-2 (filename)
+  (checksum (apply #'compact-2 (parse-disk-map (uiop:read-file-line filename)))))
+
+
+(part-2 "input.txt") ; => 6636608781232 (43 bits, #x60934B57BB0)
