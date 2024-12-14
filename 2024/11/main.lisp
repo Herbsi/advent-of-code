bb3e7bd95dd1c161eba09cf145226a640fef3630
Herbsi <git@hoehenberger.me>
2024-12-14 20:05:28 +0100

    2024/11 – Part 2

--- 2024/11/main.lisp
+++ 2024/11/main.lisp
@@ -1,3 +1,5 @@
+(ql:quickload :alexandria)
+(ql:quickload :fset)
 (ql:quickload :iterate)
 (ql:quickload :str)
 (ql:quickload :trivia)
@@ -28,23 +30,51 @@
 
 (defun change-stone (stone)
   (trivia:match (number-to-digits stone)
-    ((list 0) (list 1))
+    ((list 0) (list (list 1 stone)))
     ((trivia:guard digits
                    (evenp (length digits)))
-     (list (digits-to-number (subseq digits 0 (/ (length digits) 2)))
-           (digits-to-number (subseq digits (/ (length digits) 2)))))
-    (_ (list (* stone 2024)))))
+     (list (list (digits-to-number (subseq digits 0 (/ (length digits) 2))) stone)
+           (list (digits-to-number (subseq digits (/ (length digits) 2))) stone)))
+    (_ (list (list (* stone 2024) stone)))))
 
 
 (defun blink (stones)
   (mapcan #'change-stone stones))
 
 
-(defun part-1 (filename)
+(defun part-1 (filename blinks)
   (iter
-    (for blinks from 0 below 25)
+    (for blink from 0 below blinks)
     (for stones initially (parse-stones filename) then (blink stones))
     (finally (return (length stones)))))
 
 
-(part-1 "input.txt") ; => 220999 (18 bits, #x35F47)
+(part-1 "input.txt" 25) ; => 220999 (18 bits, #x35F47)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defun part-2 (filename blinks)
+  (iter
+    (for blink from 0 below blinks)
+
+    (for counts next (make-hash-table))
+    (for old-counts previous counts initially (make-hash-table))
+
+    (if-first-time
+     (iter
+       (for stone in (parse-stones filename))
+       (incf (gethash stone old-counts 0))))
+
+    (for stones next (alexandria:hash-table-keys old-counts))
+    (iter
+      (for (stone origin) in (blink stones))
+      (incf (gethash stone counts 0)
+            (gethash origin old-counts)))
+
+    (finally (return (iter
+                       (for (stone count) in-hashtable counts)
+                       (summing count))))))
+
+
+(part-2 "input.txt" 75) ; => 261936432123724 (48 bits, #xEE3AD3FD8F4C)
