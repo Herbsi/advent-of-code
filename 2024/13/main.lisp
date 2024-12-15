2799fb9a2a541b9c70426bd927501d6cf233c6f0
Herbsi <git@hoehenberger.me>
2024-12-15 21:43:40 +0100

    2024/13 – Part 2

--- 2024/13/main.lisp
+++ 2024/13/main.lisp
@@ -54,3 +54,54 @@
 
 
 (part-1 "input.txt") ; => 32026 (15 bits, #x7D1A)
+
+
+(let ((ptrn (ppcre:create-scanner "Prize: X=(\\d+), Y=(\\d+)")))
+  (defun parse-prize-2 (string)
+    (ppcre:register-groups-bind
+        ((#'parse-integer x y))
+        (ptrn string)
+      (list 'prize (+ x 10000000000000) (+ y 10000000000000)))))
+
+
+(defun parse-machine-2 (machine)
+  (list (parse-button (car machine))
+        (parse-button (cadr machine))
+        (parse-prize-2 (caddr machine))))
+
+
+(defun parse-input-2 (filename)
+  (iter
+    (for machine on (uiop:read-file-lines filename) by #'cddddr)
+    (collect (parse-machine-2 machine))))
+
+
+(defun solve-machine-2 (machine)
+  (let* ((button-a (car machine))
+         (button-b (cadr machine))
+         (prize (caddr machine))
+         (det (- (* (cadr button-a) (caddr button-b))
+                 (* (cadr button-b) (caddr button-a))))
+         (a-pushes (and (not (zerop det))
+                        (/ (- (* (caddr button-b) (cadr prize))
+                              (* (cadr button-b) (caddr prize)))
+                           det)))
+         (b-pushes (and (not (zerop det))
+                        (/ (- (* (cadr button-a) (caddr prize))
+                              (* (caddr button-a) (cadr prize)))
+                           det))))
+    (and (not (zerop det))
+         (integerp a-pushes) (integerp b-pushes)
+         (<= 0 a-pushes) (<= 0 b-pushes)
+         (+ (* 3 a-pushes) (* 1 b-pushes)))))
+
+
+(defun part-2 (filename)
+  (iter
+    (for machine in (parse-input-2 filename))
+    (for tokens = (solve-machine-2 machine))
+    (when tokens
+      (summing tokens))))
+
+
+(part-2 "input.txt") ; => 89013607072065 (47 bits, #x50F5186E0141)
